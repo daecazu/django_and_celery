7f2b154b7d53afe45619ac211104919206eb8245
daecazu <daecazu@gmail.com>
2018-05-15 18:32:07 -0500

    admin fuction, and updated task

--- tareas/admin.py
+++ tareas/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .tasks import send_users_emails
+class UserAdmin(UserAdmin):
 
-# Register your models here.
+
+    actions =['send_emails_action',]
+
+    def send_emails_action(self,request,queryset):
+        send_users_emails.delay()
+        filas_actualizadas=queryset.update(is_staff=True)
+        return True
+
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
--- tareas/tasks.py
+++ tareas/tasks.py
@@ -2,11 +2,11 @@ from celery import Celery, shared_task
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from tutorialcelery import settings
-
+@shared_task
 def send_users_emails():
     subject = 'mensaje de prueba'
     message = ' Bienvenido esto es un mensaje de prueba Celery, rabbitmq y django'
-    users = User.objects.filter(username='danielk')
+    users = User.objects.all()
     for user in users:
         send_mail(subject,message,settings.EMAIL_HOST_USER,[user.email],fail_silently=False)
 
